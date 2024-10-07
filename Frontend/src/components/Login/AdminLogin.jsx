22fff22729ad89e2bf1b063329c420daa84efae1
Divendra yadav <divendra.singhyadav.phe23@itbhu.ac.in>
2024-10-07 20:00:33 +0530

    headache error

--- Frontend/src/components/Login/AdminLogin.jsx
+++ Frontend/src/components/Login/AdminLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import url from '../axios';
+import url from '../axios.jsx';
 // import axios from 'axios';
 
 import { HiEye, HiEyeOff } from 'react-icons/hi'; // Import HiEye and HiEyeOff icons
