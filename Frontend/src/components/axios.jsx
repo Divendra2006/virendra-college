f5040214d979a6f44c3df4a35a40c5cbffe9f66e
Divendra yadav <divendra.singhyadav.phe23@itbhu.ac.in>
2024-10-08 14:32:49 +0530

    abc

--- Frontend/src/components/axios.jsx
+++ Frontend/src/components/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const url = axios.create({
-    baseURL : "http://localhost:8000",
+    baseURL : "https://virendra-college.onrender.com",
     withCredentials:true
 })
 
